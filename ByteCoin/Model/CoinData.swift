629ede29284e31600d5944c4219c8d0248ec1921
PorkStew <stewartclay166@gmail.com>
2022-09-07 01:38:55 -0700

    uploading project

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Stewart Clay on 2022/09/06.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Decodable {
+    let rate: Double
+}
